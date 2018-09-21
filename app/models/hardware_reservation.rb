e7931167a2ebd0e71348a617e26074f6140726c5
Evgeny Druzhinin <edruhinin@gmail.com>
2018-09-21 16:03:04 +0000

    Отображение занятости оборудования

--- app/models/hardware_reservation.rb
+++ app/models/hardware_reservation.rb
@@ -1,7 +1,11 @@
 class HardwareReservation < ActiveRecord::Base
 	belongs_to :hardware
 	belongs_to :user
+	validates :to_time, presence: true
+	validates :from_time, presence: true
+	validates :name, presence: true
 	
 	scope :active,lambda { where('to_time >= ?',Time.now )}
 	scope :occupied, lambda { where('from_time <= ? and ? <= to_time',Time.now,Time.now)}
+	scope :occupied_nextweek, lambda { where('from_time > ? and from_time < ?',Time.now,1.week.from_now)}
 end
