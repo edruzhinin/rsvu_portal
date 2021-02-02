4b6740b0ff9badbd01f85f9de4d264efb7b12d69
Evgeny Druzhinin <edruhinin@gmail.com>
2021-02-02 21:48:39 +0000

    Добавлено отображение истории мероприятий

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,6 @@ class Event < ActiveRecord::Base
   has_many :reservations
   
   scope :active,lambda { where('to_time >= ?',Time.now.to_date)}
+  scope :notactive,lambda { where('to_time < ?',Time.now.to_date)}
   
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -30,6 +30,7 @@ class Reservation < ActiveRecord::Base
 	validates_with ReservationValidator
   
   scope :active, -> {joins(:event).where('DATE(events.to_time) >= ?',Time.now.to_date)}
+  scope :old, -> {joins(:event).where('DATE(events.to_time) < ?',Time.now.to_date)}
   
   scope :occupied, -> {joins(:event).where('DATE(events.from_time) <= ? and ? <= DATE(events.to_time)',Time.now.to_date,Time.now.to_date)}
 	scope :occupied_nextweek, -> {joins(:event).where('DATE(events.from_time) > ? and DATE(events.from_time) < ?',Time.now.to_date,1.week.from_now.to_date)}
