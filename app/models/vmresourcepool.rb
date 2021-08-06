02658421b849593e4728bb7b99fca00cfdc13805
Evgeny Druzhinin <edruhinin@gmail.com>
2021-08-06 05:58:08 +0000

    Отображение количества Vm для ResourcePool

--- app/models/vmresourcepool.rb
+++ app/models/vmresourcepool.rb
@@ -12,6 +12,29 @@ class Vmresourcepool < ActiveRecord::Base
 			return "Не используется"
 		end
 	end
+
+	def get_vm_count
+		vm_count = 0
+		
+		child_vmresourcepool.each do |rpool|
+			vm_count = vm_count + rpool.get_vm_count
+		end
+		
+		vm_count = vm_count + vms.count
+		return vm_count
+	end
+
+	def get_active_vm_count
+		vm_count = 0
+		
+		child_vmresourcepool.each do |rpool|
+			vm_count = vm_count + rpool.get_active_vm_count
+		end
+		
+		vm_count = vm_count + vms.where("state=1").count
+		return vm_count
+	end
+
   
   def Vmresourcepool.status_to_s(_Status)
 		case _Status
